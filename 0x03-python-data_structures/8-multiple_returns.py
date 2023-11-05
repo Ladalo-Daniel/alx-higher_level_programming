72251b853375c6f0560a88960d0555081d2b8a6c
Ladalo-Daniel <baladalo@gmail.com>
2023-11-05 08:22:04 -0800

    critical reviews and fixs

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,14 +4,3 @@ def multiple_returns(sentence):
     first_char = sentence[0] if length > 0 else "None"
     tup = length, first_char
     return(tup)
-
-vi 9-max_integer.py
-#!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
-    for i in my_list:
-        if i > x:
-            x = i
-    return (x)
